4c9f32e90336feec9834d1bb7c9481a15761b288
Grant Arnold <grant.arnold@safe.com>
2016-08-31 22:32:24 -0700

    Change volume type to bind

--- start_containers.ps1
+++ start_containers.ps1
@@ -5,5 +5,5 @@ $scriptDir = $scriptDir -replace "([A-z]):", '/$1'
 
 # create services
 docker swarm init
-docker service create --name fme_engine -p 7777:7777 --mount type=volume,target=/fmeengine/workspaces,source=$scriptDir/workspaces --mount type=volume,target=/fmeengine/results,source=$scriptDir/results --mount type=volume,target=/fmeengine/logs,source=$scriptDir/logs safesoftware/fme-engine-2017
-docker service create --name web -p 80:80 --mount type=volume,target=/usr/local/apache2/htdocs,source=$scriptDir/results httpd:alpine
+docker service create --name fme_engine -p 7777:7777 --mount type=bind,target=/fmeengine/workspaces,source=$scriptDir/workspaces --mount type=bind,target=/fmeengine/results,source=$scriptDir/results --mount type=bind,target=/fmeengine/logs,source=$scriptDir/logs safesoftware/fme-engine-2017
+docker service create --name web -p 80:80 --mount type=bind,target=/usr/local/apache2/htdocs,source=$scriptDir/results httpd:alpine
--- start_containers.sh
+++ start_containers.sh
@@ -3,5 +3,5 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker swarm init
-docker service create --name fme_engine -p 7777:7777 --mount type=volume,target=/fmeengine/workspaces,source=$SCRIPTPATH/workspaces --mount type=volume,target=/fmeengine/results,source=$SCRIPTPATH/results --mount type=volume,target=/fmeengine/logs,source=$SCRIPTPATH/logs safesoftware/fme-engine-2017
-docker service create --name web -p 80:80 --mount type=volume,target=/usr/local/apache2/htdocs,source=$SCRIPTPATH/results httpd:alpine
+docker service create --name fme_engine -p 7777:7777 --mount type=bind,target=/fmeengine/workspaces,source=$SCRIPTPATH/workspaces --mount type=bind,target=/fmeengine/results,source=$SCRIPTPATH/results --mount type=bind,target=/fmeengine/logs,source=$SCRIPTPATH/logs safesoftware/fme-engine-2017
+docker service create --name web -p 80:80 --mount type=bind,target=/usr/local/apache2/htdocs,source=$SCRIPTPATH/results httpd:alpine
